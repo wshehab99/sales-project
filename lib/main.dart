36bf20284beb44c8c2cbf3564f8521e2b5db02e7
Waleed Bin-Shehab <57075865+wshehab99@users.noreply.github.com>
2021-07-06 01:00:13 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
-
+//import './modules/product.dart';
 import './pages/auth.dart';
 import './pages/products_admin.dart';
 import './pages/products.dart';
 import './pages/product.dart';
+import 'package:scoped_model/scoped_model.dart';
+import './scoped-model/Main.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -21,59 +23,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String, dynamic>> _products = [];
-
-  void _addProduct(Map<String, dynamic> product) {
-    setState(() {
-      _products.add(product);
-    });
-    print(_products);
-  }
-
-  void _deleteProduct(int index) {
-    setState(() {
-      _products.removeAt(index);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // debugShowMaterialGrid: true,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple,
-        buttonColor: Colors.deepPurple,
-      ),
-      // home: AuthPage(),
-      routes: {
-        '/': (BuildContext context) => AuthPage(),
-        '/products': (BuildContext context) => ProductsPage(_products),
-        '/admin': (BuildContext context) =>
-            ProductsAdminPage(_addProduct, _deleteProduct),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        final List<String> pathElements = settings.name.split('/');
-        if (pathElements[0] != '') {
-          return null;
-        }
-        if (pathElements[1] == 'product') {
-          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-            builder: (BuildContext context) => ProductPage(
-                _products[index]['title'],
-                _products[index]['image'],
-                _products[index]['price'],
-                _products[index]['description']),
-          );
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute(
-            builder: (BuildContext context) => ProductsPage(_products));
-      },
-    );
+    return ScopedModel<MainModel>(
+        model: MainModel(),
+        child: MaterialApp(
+          title: 'Sales',
+          // debugShowMaterialGrid: true,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepPurple,
+            buttonColor: Colors.deepPurple,
+          ),
+          // home: AuthPage(),
+          routes: {
+            '/': (BuildContext context) => AuthPage(),
+            '/products': (BuildContext context) => ProductsPage(),
+            '/admin': (BuildContext context) => ProductsAdminPage(),
+          },
+          onGenerateRoute: (RouteSettings settings) {
+            final List<String> pathElements = settings.name.split('/');
+            if (pathElements[0] != '') {
+              return null;
+            }
+            if (pathElements[1] == 'product') {
+              final int index = int.parse(pathElements[2]);
+              return MaterialPageRoute<bool>(
+                  builder: (BuildContext context) => ProductPage(index));
+            }
+            return null;
+          },
+          onUnknownRoute: (RouteSettings settings) {
+            return MaterialPageRoute(
+                builder: (BuildContext context) => ProductsPage());
+          },
+        ));
   }
 }
